6163f3ae4c2e25f52e72c2e25433d8b4edb325f2
Willian Spalaor <willianspalaor@gmail.com>
2023-07-08 05:02:40 -0300

    Update server.js

--- notify-app-server/server.js
+++ notify-app-server/server.js
@@ -1,7 +1,6 @@
 const http = require('http');
 const server = http.createServer();
 const cors = require('cors');
-const connectedUsers = {}; // Objeto para armazenar os sockets conectados
 
 const io = require('socket.io')(server, {
 	cors: {
@@ -13,34 +12,8 @@ const io = require('socket.io')(server, {
 });
 
 io.on('connection', (socket) => {
-  
-  	// Verifica se já existe um socket com o mesmo id da query
-	if (socket.handshake.query.id && connectedUsers[socket.handshake.query.id]) {
-
-		// Atualiza as informações do socket existente
-		let existingSocket = connectedUsers[socket.handshake.query.id];
-		existingSocket.socketId = socket.id;
-		existingSocket.userId = socket.handshake.query.userId;
-
-	} else {
-
-		// Adiciona o novo socket ao objeto connectedUsers
-		connectedUsers[socket.handshake.query.id] = {
- 			socketId: socket.id,
-	 		userId: socket.handshake.query.userId
-		};
-	}
-
   	socket.on('notify', (data) => {
-
-  		let id = data['id'];
-
-  		if(connectedUsers[id]){
-  			let targetSocket = io.sockets.sockets.get(connectedUsers[id].socketId);
-  			io.to(targetSocket.id).emit('newMessage', data);
-
-  			console.log("emitted new message");
-  		}
+		io.emit('newMessage', data);
  	});
 });
 
